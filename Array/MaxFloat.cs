d5b732186fcb144860ce45523ff7880e1891c136
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-29 01:00:16 +0530

    [Rahmath]Add: Finding Maximum Value In Float Array

---
+++ Array/MaxFloat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+     public class MaxFloat
+    {
+        public void Toprint(float[] arr)
+        {
+            float max = 0.0f;
+            if (arr[0] > arr[1] && arr[0] > arr[2] && arr[0] > arr[3] && arr[0] > arr[4])
+            {
+                max = arr[0];
+            }
+            if (arr[1] > arr[0] && arr[1] > arr[2] && arr[1] > arr[3] && arr[1] > arr[4])
+            {
+                max = arr[1];
+            }
+            if (arr[2] > arr[0] && arr[2] > arr[1] && arr[2] > arr[3] && arr[2] > arr[4])
+            {
+                max = arr[2];
+            }
+            if (arr[3] > arr[0] && arr[3] > arr[1] && arr[3] > arr[2] && arr[3] > arr[4])
+            {
+                max = arr[3];
+            }
+            if (arr[4] > arr[0] && arr[4] > arr[1] && arr[4] > arr[2] && arr[4] > arr[3])
+            {
+                max = arr[4];
+            }
+            Console.WriteLine("Maximum float value in an array is: " + max);
+        }
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -6,9 +6,13 @@ namespace Array
         static void Main(string[] args)
         {
             int[] intArr = { 54,890,111,67,45};
+            float[] floatArr = { 12.6f, 33.8f, 12.06f, 9.32f, 5f };
             MaxInt array = new MaxInt();
             Console.WriteLine("Maximum value in an Array is:");
             array.Toprint(intArr);
+            MaxFloat array2 = new MaxFloat();
+            Console.WriteLine("Maximum value in a float is:");
+            array2.Toprint(floatArr);
         }
     }
 }
